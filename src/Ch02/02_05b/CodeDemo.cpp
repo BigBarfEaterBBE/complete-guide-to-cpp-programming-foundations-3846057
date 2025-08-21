a15ae6db23b9518bb4f6d54f57a2af879d4a4044
BigBarfEaterBBE <ajiang9@bostonk12.org>
2025-08-21 20:09:50 +0000

    jnjn

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,9 +3,18 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+int a,b = 5;
 int main(){
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,18 @@
 #include <cstdint>
 
 int main(){
-    
+    float target_x;
+    int32_t sprite_x;
+    uint32_t player_x;
+
+    target_x = -132.45f;
+    sprite_x = target_x;
+    player_x = sprite_x;
+
+    std::cout << "Target X (float): " << target_x << std::endl;
+    std::cout << "Target X (int32_t): " << sprite_x << std::endl;
+    std::cout << "Target X (uint32_t): " << player_x << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
